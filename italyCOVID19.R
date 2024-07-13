7af2b7005eea16eb0735787440dacaef134790c2
Bryce <bcars268@mtroyal.ca>
2024-07-13 00:37:50 -0600

    Change course of algorithm
    The algorithm previously used a different paramete restimation methodology; now it is using the Residual Sum of Squares methodology.
    
    Further, Ashok has reverted to using a simpler SIRD model function and removed much of the previous code.
    
    Some of the changes include renaming variables, other changes simply remove code and take an entirely different, interactive approach to using R and working with the R code herein.
    
    Co-Authored-By: Ashok Krishnamurthy <ashokkrish@gmail.com>

--- italyCOVID19.R
+++ italyCOVID19.R
@@ -27,9 +27,9 @@ covid19.regional <- function(country = "Italy",
   if (!country %in% countryNamesInEnglish)
     errorCondition(paste("Argument `country` was not given in English,",
                          "or is not a recognized country."))
-
+  
   message("`country` was valid; assuming all other arguments are valid.")
-
+  
   regionalCOVID19Data <-
     covid19(country, 2, startDate, endDate, verbose = FALSE) %>%
     select(date,
@@ -46,13 +46,13 @@ covid19.regional <- function(country = "Italy",
     filter(region == subregion) %>%
     mutate(date = as.Date(date)) %>%
     arrange(date)
-
+  
   if(nrow(regionalCOVID19Data) == 0) {
     errorCondition("No data available for selected region.")
   }
-
+  
   regionalPopulation <- first(regionalCOVID19Data$population)
-
+  
   message(sprintf(r"[Spatial information:
 Country = %s
 Region = %s
@@ -63,6 +63,8 @@ subregion,
 first(regionalCOVID19Data$GADM),
 prettyNum(regionalPopulation, big.mark = ",")))
 
+print(prettyNum(regionalPopulation, big.mark = ","))
+
   as_tibble(regionalCOVID19Data) %>%
     select(date, confirmed, deaths, recovered) %>%
     rename(dead = deaths) %>%
@@ -72,194 +74,113 @@ prettyNum(regionalPopulation, big.mark = ",")))
            prevalence = confirmed - recovered - dead) %>%
     # Correct the prevalence column based on "the formula".
     mutate(#prevalence = lag(prevalence) + infections - recoveries - deaths,
-           susceptible = mapply(sum,
-                                regionalPopulation,
-                                -infections,
-                                -recoveries,
-                                -deaths),
-           .keep = "all") %>%
+      susceptible = mapply(sum,
+                           regionalPopulation,
+                           -infections,
+                           -recoveries,
+                           -deaths),
+      .keep = "all") %>%
     slice(-1)
 }
 
-## Define the function to solve the SIRD model.
-sird <- function(t = seq(0, 90),
-                 ## Cumulative value of the compartment at the outset of the
-                 ## simulation.
-                 susceptible = 499,
-                 prevalence = 1,
-                 confirmed = 1,
-                 recovered = 0,
-                 dead = 0,
-                 deaths = 0,
-                 recoveries = 0,
-
-                 ## Parameters
-                 beta = 5e-3,
-                 gamma = 1e-3,
-                 delta = 3e-4) {
-  lsoda(
-    c(
-      S = susceptible,
-      I = prevalence, #confirmed,
-      R = recovered,
-      D = dead
-    ),
-    t,
-    function(t, y, parms, ...) {
-      ## lsoda's documentation is out of date, as the names of y are not
-      ## available inside func, so we construct an environment from some data
-      ## ourselves. ***This is the slowest part of this function, taking ~130ms
-      ## while the expression evaluated inside this data environment only takes
-      ## ~80ms.*** It is commented for understandability, and the optimized,
-      ## less readable version is preferred for speed.
-      # with(as.list(c(y, parms)), {
-      #   N <- S + I + R
-      #   βSIN <- beta * S * I / N
-      #   γI <- gamma * I
-      #   δI <- delta * I
-      #   list(c(-βSIN,          # dS/dt
-      #          βSIN - γI - δI, # dI/dt
-      #          γI,             # dR/dt
-      #          δI),            # dD/dt
-      #        population = N)
-      # })
-      N <- y[1] + y[2] + y[3]
-      βSIN <- parms[1] * y[1] * y[2] / N
-      γI <- parms[2] * y[2]
-      δI <- parms[3] * y[2]
-      list(c(-βSIN, # dS/dt
-             βSIN - γI - δI, # dI/dt
-             γI, # dR/dt
-             δI), # dD/dt
-           "population" = N)
-    },
-    parms = c(beta, gamma, delta),
-  ) %>%
-  as.data.frame() %>%
-  as_tibble() %>%
-  rename(susceptible = S,
-         prevalence = I,
-         #confirmed = I,
-         recovered = R,
-         dead = D,
-         population = population.S)
-}
-
-## The observed, cumulative numbers.
 observed <- covid19.regional() %>%
-  select(susceptible, confirmed, recovered, dead, prevalence, deaths, recoveries, deaths)
+  select(susceptible, confirmed, recovered, dead, prevalence, deaths, recoveries, date)
 
 observed
 
-expected <- select(do.call(sird, first(observed)),
-                   !c(time, population))
-
-expected
-
-## FIXME: `optimx` hates this, and refuses to run when the square of the
-## differences is used.
-objfun <- function(par) {
-  expected <- do.call(sird, as.list(par))
-  ##sum((observed$prevalence - expected$prevalence)^2, na.rm = TRUE)
-  sum(observed^2, -(expected^2), na.rm = TRUE)
+# Define the SIRD model
+SIRD <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    N <- S + I + R
+    dS <- -beta * S * I / N
+    dI <- beta * S * I / N - gamma * I - delta * I
+    dR <- gamma * I
+    dD <- delta * I
+    list(c(dS, dI, dR, dD))
+  })
 }
 
-## Using educated guesses at parameter values for the deterministic simulation,
-## obtain an estimate for each parameter using optimx.
-parameterEstimates <-
-  optimx(
-    par = c(
-      beta = 0.09,
-      gamma = 0.02,
-      delta = 0.001
-    ),
-    fn = objfun,
-    method = "L-BFGS-B",
-    lower = c(0, 0, 0)
-  )
+# Initial state values
+init <- c(S = observed$susceptible[1], I = observed$prevalence[1], R = observed$recovered[1], D = observed$dead[1])
 
-parameterEstimates
+# Time vector
+times <- seq(1, nrow(observed), by = 1)
 
-fitted <-
-  with(first(observed),
-       sird(susceptible = susceptible,
-            prevalence = prevalence,
-            confirmed = confirmed,
-            recovered = recovered,
-            dead = dead,
-            deaths = deaths,
-            recoveries = recoveries,
-            beta = parameterEstimates$beta,
-            gamma = parameterEstimates$gamma,
-            delta = parameterEstimates$delta)) #%>%
-  ## FIXME: I have ABSOLUTLEY no idea why confirmed isn't the cumulative sum
-  ## here, because the value returned by `sird` should be the cumulative sum! It
-  ## is for expected! That's what I programmed! What?!
-  #mutate(confirmed = cumsum(confirmed))
+# Objective function to minimize the residual sum of squares (RSS)
+RSS <- function(parameters) {
+  names(parameters) <- c("beta", "gamma", "delta")
+  out <- lsoda(y = init, times = times, func = SIRD, parms = parameters)
+  sum((observed$susceptible - out[, "S"])^2 +
+        (observed$prevalence - out[, "I"])^2 +
+        (observed$recovered - out[, "R"])^2 +
+        (observed$dead - out[, "D"])^2)
+}
 
-fitted
+# Initial guesses for parameters
+initial_guesses <- list(
+  c(beta = 0.4, gamma = 0.1, delta = 0.01),
+  c(beta = 0.3, gamma = 0.1, delta = 0.02),
+  c(beta = 0.5, gamma = 0.1, delta = 0.03)
+)
 
-# Ashok's plotting code ---------------------------------------------------
-stopifnot(nrow(observed) == nrow(fitted))
-
-times <- seq_along(observed$dead)
-
-plotData <-
-  tibble(
-    date = times,
-    observed_I = observed$confirmed,
-    observed_R = observed$recovered,
-    observed_D = observed$dead,
-    fitted_I = fitted$prevalence, #fitted$confirmed,
-    fitted_R = fitted$recovered,
-    fitted_D = fitted$dead
+fit_optimization <- function(init_params) {
+  fit <- tryCatch(
+    {
+      optim(par = init_params, fn = RSS, method = "L-BFGS-B", lower = c(0, 0, 0), upper = c(1, 1, 1))
+    },
+    error = function(e) {
+      message("Optimization error: ", e)
+      NULL
+    }
   )
   
-plot(
-  times,
-  fitted$confirmed,
-  col = "blue",
-  type = "l",
-  lwd = 2,
-  bty = "l",
-  xlab = "Time",
-  ylab = "Compartment Size",
-  main = "IRD Epidemic Compartments",
-  ## MAYBE FIXME: if the sum of susceptible, confirmed, recovered, and dead is
-  ## used then the population is inflated... why?
-  ylim = c(0, summarize(first(covid19.regional()),
-                        population = sum(susceptible,
-                                         infections,
-                                         recoveries,
-                                         deaths))$population)
-)
-lines(times, fitted$recovered, lwd = 2, col = "darkgreen")
-lines(times, fitted$dead, lwd = 2, col = "red")
-abline(v = 0, h = 0)
+  if (!is.null(fit) && fit$convergence == 0) {
+    return(fit$par)
+  } else {
+    return(NULL)
+  }
+}
 
-# Plot observed vs fitted values
-p1 <- ggplot(plotData, aes(x = date)) +
-  geom_line(aes(y = observed_I, color = "Observed Prevalence")) +
-  geom_line(aes(y = fitted_I, color = "Fitted Prevalence")) +
-  labs(y = "Prevalence", color = "Legend") +
+opt_params <- NULL
+for (guess in initial_guesses) {
+  opt_params <- fit_optimization(guess)
+  if (!is.null(opt_params)) break
+}
+
+if (is.null(opt_params)) {
+  stop("Optimization did not converge with any initial guesses.")
+}
+
+# Print the best-fit parameters
+opt_params
+
+#Returns a delta of 0.00000000
+
+# Solve the SIRD model with the optimized parameters
+out <- lsoda(y = init, times = times, func = SIRD, parms = opt_params)
+
+out
+
+# Convert the output to a data frame for plotting
+output <- as.data.frame(out)
+output$date <- observed$date
+
+# Plot the results
+observed_long <- observed %>% 
+  select(-susceptible, -confirmed) %>% 
+  pivot_longer(cols = -date, names_to = "compartment", values_to = "observed")
+
+output_long <- output %>%
+  select(-S) %>%
+  pivot_longer(cols = -date, names_to = "compartment", values_to = "predicted")
+
+plot_data <- full_join(observed_long, output_long, by = c("date", "compartment"))
+
+ggplot(plot_data, aes(x = date)) +
+  geom_line(aes(y = observed, color = compartment), linewidth = 1) +
+  geom_line(aes(y = predicted, color = compartment), linetype = "dashed", linewidth = 1) +
+  labs(title = "SIRD Model Fit to COVID-19 Data",
+       x = "Date",
+       y = "Count",
+       color = "Compartment") +
   theme_minimal()
-
-p1 
-
-p2 <- ggplot(plotData, aes(x = date)) +
-  geom_line(aes(y = observed_R, color = "Observed Cumulative Recovered")) +
-  geom_line(aes(y = fitted_R, color = "Fitted Recovered")) +
-  labs(y = "Recovered", color = "Legend") +
-  theme_minimal()
-
-p2
-
-p3 <- ggplot(plotData, aes(x = date)) +
-  geom_line(aes(y = observed_D, color = "Observed Cumulative Deaths")) +
-  geom_line(aes(y = fitted_D, color = "Fitted Deaths")) +
-  labs(y = "Deaths", color = "Legend") +
-  theme_minimal()
-
-p3
-
-grid.arrange(p1, p2, p3, ncol = 1)
